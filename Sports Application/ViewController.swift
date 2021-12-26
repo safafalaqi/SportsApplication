4e453735fc4c9e98769cbdc7016308681039a463
Safa Falaqi <sfalaqi@gmail.com>
2021-12-26 23:39:36 +0300

    Initial Commit

---
+++ Sports Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sports Application
+//
+//  Created by Safa Falaqi on 26/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
